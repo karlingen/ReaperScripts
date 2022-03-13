508bd1b5c2913722217e95d46c203ab3d4dfb5d1
Karl Metum <karlo.m@gmail.com>
2022-03-13 23:08:47 +0100

    Add RiftFilterLite script

---
+++ Add RiftFilterLite with Cutoff Envelope.lua
@@ -0,0 +1,17 @@
+-- @version 1.0
+-- @author karlingen
+-- @description Add Cutoff Envelope for RiftFilterLite
+-- @website https://github.com/karlingen
+
+function RunScript()
+    local fxName = "Rift Filter Lite"
+    local selectedTracks = reaper.CountSelectedTracks(0)
+    for i = 0, selectedTracks - 1 do
+        local track = reaper.GetSelectedTrack(0, i)
+        local fxNumber = reaper.TrackFX_AddByName(track, fxName, false, 1)
+        local cutoffParamIndex = 1
+        local trackEnvelope = reaper.GetFXEnvelope(track, fxNumber, cutoffParamIndex, true)
+    end
+end
+
+RunScript()
