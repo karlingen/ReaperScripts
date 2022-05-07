6e9b4c6937c6b0f883866f75cc5e512ab48c8522
Karl Metum <karlo.m@gmail.com>
2022-05-07 14:25:45 +0200

    Copy over more parameters to the new track

--- Bounce In Place.lua
+++ Bounce In Place.lua
@@ -3,18 +3,22 @@
 -- @description Bounce In Place
 -- @website https://github.com/karlingen
 
-function Act(id) reaper.Main_OnCommand(id, 0) end
+function Act(id)
+    reaper.Main_OnCommand(id, 0)
+end
 
 function RunScript()
     local item = reaper.GetSelectedMediaItem(0, 0)
-    if not item then return end
+    if not item then
+        return
+    end
     UnselectAllItems()
 
     reaper.SetMediaItemSelected(item, true)
     ApplyTakeFX()
 
     -- Grab the media item state after the FX have been applied to it.
-    local _, itemStateChunk = reaper.GetItemStateChunk(item, '', true)
+    local _, itemStateChunk = reaper.GetItemStateChunk(item, "", true)
     local mediaItemTrack = reaper.GetMediaItemTrack(item)
     local currentVolume = reaper.GetMediaTrackInfo_Value(mediaItemTrack, "D_VOL")
     local currentTrackId = reaper.CSurf_TrackToID(mediaItemTrack, false)
@@ -43,8 +47,11 @@ function RunScript()
 
     CropToActiveTakeInItems()
 
-    -- Set the correct volume
-    reaper.SetMediaTrackInfo_Value(newTrack, "D_VOL", currentVolume)
+    -- Set the name for the new track
+    local trackName = reaper.GetTrackState(mediaItemTrack) .. " BIP"
+    reaper.GetSetMediaTrackInfo_String(newTrack, "P_NAME", trackName, true)
+
+    CopyTrackValues(mediaItemTrack, newTrack)
 
     reaper.SetOnlyTrackSelected(newTrack)
     reaper.UpdateArrange()
@@ -70,8 +77,64 @@ function DeleteActiveTakeFromItems()
     Act(40129)
 end
 
+function CopyTrackValues(FromTrack, ToTrack)
+    trackInfoKeys = {
+        "B_MUTE",
+        "B_PHASE",
+        "IP_TRACKNUMBER",
+        "I_SOLO",
+        "I_FXEN",
+        "I_RECARM",
+        "I_RECINPUT",
+        "I_RECMODE",
+        "I_RECMON",
+        "I_RECMONITEMS",
+        "I_AUTOMODE",
+        "I_NCHAN",
+        "I_SELECTED,",
+        "I_WNDH",
+        "I_TCPH",
+        "I_TCPY",
+        "I_MCPX",
+        "I_MCPY",
+        "I_MCPW",
+        "I_MCPH",
+        "I_FOLDERDEPTH",
+        "I_FOLDERCOMPACT",
+        "I_MIDIHWOUT",
+        "I_PERFFLAGS",
+        "I_CUSTOMCOLOR",
+        "I_HEIGHTOVERRIDE",
+        "B_HEIGHTLOCK",
+        "D_VOL",
+        "D_PAN",
+        "D_WIDTH",
+        "D_DUALPANL",
+        "D_DUALPANR",
+        "I_PANMODE",
+        "D_PANLAW",
+        "P_ENV",
+        "B_SHOWINMIXER",
+        "B_SHOWINTCP",
+        "B_MAINSEND",
+        "C_MAINSEND_OFFS",
+        "B_FREEMODE",
+        "C_BEATATTACHMODE",
+        "F_MCP_FXSEND_SCALE",
+        "F_MCP_SENDRGN_SCALE",
+        "I_PLAY_OFFSET_FLAG",
+        "D_PLAY_OFFSET"
+    }
+
+    for i = 1, #trackInfoKeys do
+        local dictKey = trackInfoKeys[i]
+        local theValue = reaper.GetMediaTrackInfo_Value(FromTrack, dictKey)
+        reaper.SetMediaTrackInfo_Value(ToTrack, dictKey, theValue)
+    end
+end
+
 reaper.Undo_BeginBlock()
 reaper.PreventUIRefresh(1)
 RunScript()
 reaper.PreventUIRefresh(-1)
-reaper.Undo_EndBlock('Bounce In Place', 0)
+reaper.Undo_EndBlock("Bounce In Place", 0)
